3109c8747cef6f5ead76857395d569c605bf26e8
Michael Ballard <ballard.michael9@gmail.com>
2020-10-10 09:51:02 +0000

    Automatically backed up by Learn

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -2,8 +2,10 @@ class SightingsController < ApplicationController
 
     def show
         @sighting = Sighting.find_by(id: params[:id])
-        render json: @sighting
+        # render json: @sighting
+
+        # mixiin data from collaborating object
     end
 
-    
+
 end
