01aca181cf256aea9d02b078a63831f7e0b0cc2c
Michael Ballard <ballard.michael9@gmail.com>
2020-10-10 09:54:52 +0000

    Automatically backed up by Learn

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -5,7 +5,10 @@ class SightingsController < ApplicationController
         # render json: @sighting
 
         # mixiin data from collaborating objects
-        render json: {id: @sighting.id, bird: @sighting.bird, location: @sighting.location}
+        # render json: {id: @sighting.id, bird: @sighting.bird, location: @sighting.location}
+
+        # even shorter syntax
+        render json: @sighting, include: [:bird, :location]
     end
 
 
